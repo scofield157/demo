1f7b1d1df8c047eca63a441b11a5f25d6d92cc46
胡子玥 <>
2019-01-29 19:27:54 +0800

    修改配置

--- conf/default.py
+++ conf/default.py
@@ -30,8 +30,8 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'hzy'
-APP_TOKEN = '7e8475a4-f36d-4375-9344-fd6ea6c6da68'
+APP_ID = 'hzy-demo'
+APP_TOKEN = '541075c8-d862-452d-b35d-63d656044ac2'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas.blueking.com:8030'
 
